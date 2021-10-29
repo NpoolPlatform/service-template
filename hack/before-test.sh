9bcbbd7ce07bc322165ee7d5afe18818484355a7
Zhao KK <kikakkz@hotmail.com>
2021-10-29 02:41:00 +0000

    Run service background

--- hack/before-test.sh
+++ hack/before-test.sh
@@ -10,5 +10,5 @@ OUTPUT=./output
 mkdir -p $OUTPUT/$PLATFORM
 for service_name in `ls $(pwd)/cmd`; do
     cp $(pwd)/cmd/$service_name/*.viper.yaml $OUTPUT/$PLATFORM
-    cd $OUTPUT/$PLATFORM; ./$service_name run
+    cd $OUTPUT/$PLATFORM; ./$service_name run > /dev/null 2>&1 &
 done
