5880a7c8a3c6d89d37eb8f51ebf72ce95e4c254d
Zhao KK <kikakkz@hotmail.com>
2021-12-17 13:16:55 +0000

    sleep 10

--- hack/before-test.sh
+++ hack/before-test.sh
@@ -12,3 +12,5 @@ for service_name in `ls $(pwd)/cmd`; do
     cp $(pwd)/cmd/$service_name/*.viper.yaml $OUTPUT/$PLATFORM
     cd $OUTPUT/$PLATFORM; ./$service_name run > /dev/null 2>&1 &
 done
+
+sleep 10
