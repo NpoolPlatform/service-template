dc5ef1a5af4f3e5554edc32a39ededf9edde9135
Zhao KK <kikakkz@hotmail.com>
2021-12-23 03:55:58 +0000

    Use new db

--- go.mod
+++ go.mod
@@ -4,9 +4,10 @@ go 1.16
 
 require (
 	entgo.io/ent v0.9.1
-	github.com/NpoolPlatform/go-service-framework v0.0.0-20211102122901-b687a4bf9b14
+	github.com/NpoolPlatform/cloud-hashing-order v0.0.0-20211220094213-6976c3eb93ee
+	github.com/NpoolPlatform/go-service-framework v0.0.0-20211222114515-4928e6cf3f1f
 	github.com/go-resty/resty/v2 v2.7.0
-	github.com/grpc-ecosystem/grpc-gateway/v2 v2.6.0
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.7.0
 	github.com/streadway/amqp v1.0.0
 	github.com/stretchr/testify v1.7.0
 	github.com/urfave/cli/v2 v2.3.0
@@ -15,7 +16,7 @@ require (
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1
 	google.golang.org/genproto v0.0.0-20211208223120-3a66f561d7aa
-	google.golang.org/grpc v1.41.0
+	google.golang.org/grpc v1.42.0
 	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.1.0
 	google.golang.org/protobuf v1.27.1
 )
--- pkg/db/db.go
+++ pkg/db/db.go
@@ -3,18 +3,30 @@ package db
 import (
 	"context"
 
-	"github.com/NpoolPlatform/go-service-app-template/pkg/db/ent"
+	"github.com/NpoolPlatform/cloud-hashing-order/pkg/db/ent"
 
-	"github.com/NpoolPlatform/go-service-framework/pkg/app"
+	"entgo.io/ent/dialect"
+	entsql "entgo.io/ent/dialect/sql"
+	"github.com/NpoolPlatform/go-service-framework/pkg/mysql"
 )
 
-var myClient *ent.Client
-
-func Init() error {
-	myClient = ent.NewClient(ent.Driver(app.Mysql().Driver))
-	return myClient.Schema.Create(context.Background())
+func client() (*ent.Client, error) {
+	conn, err := mysql.GetConn()
+	if err != nil {
+		return nil, err
+	}
+	drv := entsql.OpenDB(dialect.MySQL, conn)
+	return ent.NewClient(ent.Driver(drv)), nil
 }
 
-func Client() *ent.Client {
-	return myClient
+func Init() error {
+	cli, err := client()
+	if err != nil {
+		return err
+	}
+	return cli.Schema.Create(context.Background())
+}
+
+func Client() (*ent.Client, error) {
+	return client()
 }
