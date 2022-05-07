63755ff7cafa343ef2056956589995e63188c171
song.tang@ucloud.cn <Tangsong1>
2022-05-07 15:05:17 +0800

    fix run test

--- pkg/client/client_test.go
+++ pkg/client/client_test.go
@@ -18,17 +18,23 @@ import (
 	"github.com/google/uuid"
 )
 
-var once sync.Once
+var (
+	once              sync.Once
+	runByGithubAction bool
+	err               error
+)
 
 func TestClient(t *testing.T) {
 	once.Do(func() {
-		if runByGithubAction, err := strconv.ParseBool(os.Getenv("RUN_BY_GITHUB_ACTION")); err == nil && runByGithubAction {
-			return
-		}
+		runByGithubAction, err = strconv.ParseBool(os.Getenv("RUN_BY_GITHUB_ACTION"))
 		if err := testinit.Init(); err != nil {
 			fmt.Printf("cannot init test stub: %v\n", err)
 		}
 	})
+
+	if err == nil && runByGithubAction {
+		return
+	}
 
 	_, err := GetServiceTemplateInfoOnly(context.Background(),
 		cruder.NewFilterConds().
