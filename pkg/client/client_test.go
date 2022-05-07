8396ca3234c295a4a197f3efc377548dcd2acd6e
linuxpizi <songtang0202@gmail.com>
2022-05-07 08:55:27 +0000

    fix client test

--- pkg/client/client_test.go
+++ pkg/client/client_test.go
@@ -19,12 +19,12 @@ import (
 
 func TestClient(t *testing.T) {
 	runByGithubAction, err := strconv.ParseBool(os.Getenv("RUN_BY_GITHUB_ACTION"))
-	if err := testinit.Init(); err != nil {
-		fmt.Printf("cannot init test stub: %v\n", err)
-	}
-
 	if err == nil && runByGithubAction {
 		return
+	}
+
+	if err := testinit.Init(); err != nil {
+		fmt.Printf("cannot init test stub: %v\n", err)
 	}
 
 	_, err = GetServiceTemplateInfoOnly(context.Background(),
