bf761247e6dae0c1f47f20a3ca345d570118d38e
linuxpizi <songtang0202@gmail.com>
2022-05-07 08:14:13 +0000

    fix test

--- pkg/client/client_test.go
+++ pkg/client/client_test.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"os"
 	"strconv"
-	"sync"
 	"testing"
 
 	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
@@ -18,25 +17,17 @@ import (
 	"github.com/google/uuid"
 )
 
-var (
-	once              sync.Once
-	runByGithubAction bool
-	err               error
-)
-
 func TestClient(t *testing.T) {
-	once.Do(func() {
-		runByGithubAction, err = strconv.ParseBool(os.Getenv("RUN_BY_GITHUB_ACTION"))
-		if err := testinit.Init(); err != nil {
-			fmt.Printf("cannot init test stub: %v\n", err)
-		}
-	})
+	runByGithubAction, err := strconv.ParseBool(os.Getenv("RUN_BY_GITHUB_ACTION"))
+	if err := testinit.Init(); err != nil {
+		fmt.Printf("cannot init test stub: %v\n", err)
+	}
 
 	if err == nil && runByGithubAction {
 		return
 	}
 
-	_, err := GetServiceTemplateInfoOnly(context.Background(),
+	_, err = GetServiceTemplateInfoOnly(context.Background(),
 		cruder.NewFilterConds().
 			WithCond(constant.FieldID, cruder.EQ, structpb.NewStringValue(uuid.UUID{}.String())))
 	if err != nil {
